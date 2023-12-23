d5eedd8c8ecff274bc59b69edc61d5ea03d26169
Dmitriy Barabkin <barabkin8@gmail.com>
2023-12-23 21:13:30 +0300

    Добавьте файлы проекта.

---
+++ pyramides_z9/pyramides_z9.cpp
@@ -0,0 +1,21 @@
+﻿//Пирамидки
+#include <iostream>
+
+int intCheck(int integer) { 
+    while (std::cin.fail() || integer < 0) {
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+        std::cout << "Ошибка, вы должны ввести целое неотрицательное число." << std::endl << "Введите ещё раз: " << std::endl;
+        std::cin >> integer;
+    }
+    return integer;
+}
+
+int main()
+{
+    std::cout << "Введите количетсво пирамидок: " << std::endl;
+    unsigned int n = 0;
+    n = intCheck(n);
+
+    return 0;
+}
